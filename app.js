a834205ba8e5eaf4b1f71bce23375fb8ed37ae2b
Tomasz <sigma345@wp.pl>
2022-10-26 18:51:19 +0200

    run server and add employee list

--- app.js
+++ app.js
@@ -1,1 +1,36 @@
 const express = require('express');
+
+const app = express();
+
+app.listen(3000);
+
+const presence = 1;
+const absence = 0;
+
+const employeList = {
+    employee1 : 1,
+    employee2 : 1,
+    employee3 : 1,
+    employee4 : 1,
+    employee5 : 1,
+    employee6 : 1,
+    employee7 : 1,
+    employee8 : 1,
+    employee9 : 1,
+    employee10 : 1,
+    employee11: 1,
+    employee12 : 1,
+    employee13 : 1,
+    employee14 : 1,
+    employee15 : 1,
+    employee16 : 1,
+    employee17 : 1,
+    employee18 : 1,
+    employee19 : 1,
+    employee20 : 1
+};
+
+app.get('/', (req, res) => {
+    res.send(employeList);
+}
+);
