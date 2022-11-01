83b49961c3ac509a1de16fd679d27db83f212165
Tomasz <sigma345@wp.pl>
2022-11-01 15:39:24 +0100

    add timeRoutes, ejs

--- app.js
+++ app.js
@@ -1,75 +1,26 @@
 const express = require('express');
 const _= require('lodash');
 const mongoose = require('mongoose');
-const { object } = require('webidl-conversions');
-const EmpployeeTimeList = require('./models/workersList');
+
+const timeRoutes = require('./routes/timeRoutes');
 
 const app = express();
+
+app.set('view engine', 'ejs');
 
 const dbURI = "mongodb+srv://tomasz:tomasz@nodetut.jphc8ap.mongodb.net/node-tut?retryWrites=true&w=majority";
 mongoose.connect(dbURI)
 .then((result) => app.listen(3001))
 .catch((err) => console.log(err));
 
-const presence = 1;
-const absence = 0;
 
-const employeList = {
-    employee0 : 1,
-    employee1 : 1,
-    employee2 : 1,
-    employee3 : 1,
-    employee4 : 1,
-    employee5 : 1,
-    employee6 : 1,
-    employee7 : 1,
-    employee8 : 1,
-    employee9 : 1,
-    employee10: 1,
-    employee11 : 1,
-    employee12 : 1,
-    employee13 : 1,
-    employee14 : 1,
-    employee15 : 1,
-    employee16 : 1,
-    employee17 : 1,
-    employee18 : 1,
-    employee19 : 1
-};
+app.get('/', (req, res) => {
 
-app.get('/7', (req, res) => {
-    
-    setInterval(function() {
-    for(let i = 0; i < Object.keys(employeList).length; i++) {
-        let random = _.random(absence, presence);
-        
-        const employeeName = Object.keys(employeList)[i];
-        let presenceOfemployee = Object.values(employeList)[i] * random;
-        const employeeTimeList = new EmpployeeTimeList({
-            employee: employeeName,
-            presence: presenceOfemployee
+    res.render('index');
+});
 
-        })
-        employeeTimeList.save()
-            .then((result) => {
-                res.send(result)
-        })
-            .catch((err) => {
-            console.log(err)
-        });
-        //console.log(Object.keys(employeList)[i] + ' - ' +  presenceOfemployee);
-    }}, 60*1000);
-   
-}
-);
+app.use(timeRoutes);
 
-app.get('/15', (req, res) => {
-    
-    setInterval(function() {
-    for(let i = 0; i < Object.keys(employeList).length; i++) {
-        let random = _.random(absence, presence);
-        console.log(Object.values(employeList)[i] * random);
-    }}, 90*1000);
-    //res.send(random);
-}
-);
+app.use((req, res) => {
+    res.status(404).render('404');
+})
---
+++ routes/timeRoutes.js
@@ -0,0 +1,84 @@
+const express = require('express');
+const EmpployeeTimeList = require('../models/workersList');
+
+const router = express.Router(); 
+
+
+
+const presence = 1;
+const absence = 0;
+
+const employeList = {
+    employee0 : 1,
+    employee1 : 1,
+    employee2 : 1,
+    employee3 : 1,
+    employee4 : 1,
+    employee5 : 1,
+    employee6 : 1,
+    employee7 : 1,
+    employee8 : 1,
+    employee9 : 1,
+    employee10: 1,
+    employee11 : 1,
+    employee12 : 1,
+    employee13 : 1,
+    employee14 : 1,
+    employee15 : 1,
+    employee16 : 1,
+    employee17 : 1,
+    employee18 : 1,
+    employee19 : 1
+};
+
+router.get('/7', (req, res) => {
+    
+    setInterval(function() {
+    for(let i = 0; i < Object.keys(employeList).length; i++) {
+        let random = _.random(absence, presence);
+        
+        const employeeName = Object.keys(employeList)[i];
+        let presenceOfemployee = Object.values(employeList)[i] * random;
+        const employeeTimeList = new EmpployeeTimeList({
+            employee: employeeName,
+            presence: presenceOfemployee
+
+        })
+        employeeTimeList.save()
+            .then((result) => {
+                res.send(result)
+        })
+            .catch((err) => {
+            console.log(err)
+        });
+        
+    }}, 60*1000);
+}
+);
+
+router.get('/15', (req, res) => {
+    
+    setInterval(function() {
+    for(let i = 0; i < Object.keys(employeList).length; i++) {
+        let random = _.random(absence, presence);
+        
+        const employeeName = Object.keys(employeList)[i];
+        let presenceOfemployee = Object.values(employeList)[i] * random;
+        const employeeTimeList = new EmpployeeTimeList({
+            employee: employeeName,
+            presence: presenceOfemployee
+
+        })
+        employeeTimeList.save()
+            .then((result) => {
+                res.send(result)
+        })
+            .catch((err) => {
+            console.log(err)
+        });
+   
+    }}, 90*1000);
+}
+);
+
+module.exports = router;
